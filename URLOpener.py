430341628672a17957b2a962507931293af3f875
Sahil Varshney <savarshn@adobe.com>
2016-08-30 22:18:45 +0530

    Adding url opener script

---
+++ URLOpener.py
@@ -0,0 +1,16 @@
+import sys,webbrowser
+
+try:
+    filename=sys.argv[1]
+    count=sys.argv[2]
+except:
+    filename='links.txt'
+    count=0
+with open(filename) as f:
+    content = f.readlines()
+if count==0:
+    for urls in content:
+        webbrowser.open(urls.strip())
+else:
+    for urls in content[:count]:
+        webbrowser.open(urls.strip())
